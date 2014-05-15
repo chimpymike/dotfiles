1b458871461d8fd6349f9f5a93fae1bf3ff40e8c
Michael Callahan <chimpymike@gmail.com>
2014-05-15 17:28:07 -0400

    Move clojure configuration to a separate init file.

---
+++ home/.emacs.d/init-scripts/05_clojure.el
@@ -0,0 +1,11 @@
+;; Use paredit in clojure-mode
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
+;; Use paredit in nrepl-mode
+(add-hook 'nrepl-mode-hook 'paredit-mode)
+
+;; Use rainbow-delimiters in clojure-mode
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+
+;; Use rainbow-delimiters in nrepl-mode
+(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -9,18 +9,6 @@
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/init-scripts")
 
-;; Use paredit in clojure-mode
-(add-hook 'clojure-mode-hook 'paredit-mode)
-
-;; Use paredit in nrepl-mode
-(add-hook 'nrepl-mode-hook 'paredit-mode)
-
-;; Use rainbow-delimiters in clojure-mode
-(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-
-;; Use rainbow-delimiters in nrepl-mode
-(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
-
 ;; Use spaces for indentation
 (setq-default indent-tabs-mode nil)
 
