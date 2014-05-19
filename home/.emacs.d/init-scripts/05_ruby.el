1e3b3f8c4a29b1084eb93fc2739beb6d602a04e8
Michael Callahan <chimpymike@gmail.com>
2014-05-19 10:05:50 -0400

    Use paredit and rainbow-delimiters in ruby-mode.

--- home/.emacs.d/init-scripts/05_ruby.el
+++ home/.emacs.d/init-scripts/05_ruby.el
@@ -3,3 +3,9 @@
 
 ;; Set pry as the default ruby repl process
 (setq inf-ruby-default-implementation "pry")
+
+;; Use paredit in ruby-mode
+(add-hook 'ruby-mode-hook 'paredit-mode)
+
+;; Use rainbow-delimiters in ruby-mode
+(add-hook 'ruby-mode-hook 'rainbow-delimiters-mode)
