5c61ca6efb9492f0ea726ed2b895b34af3952c59
Mike Callahan <chimpymike@gmail.com>
2014-11-10 13:09:23 -0500

    Use paredit and rainbow delimiters in cider repl.

--- home/.emacs.d/init-scripts/05_clojure.el
+++ home/.emacs.d/init-scripts/05_clojure.el
@@ -2,10 +2,10 @@
 (add-hook 'clojure-mode-hook 'paredit-mode)
 
 ;; Use paredit in nrepl-mode
-(add-hook 'nrepl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
 
 ;; Use rainbow-delimiters in clojure-mode
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 
 ;; Use rainbow-delimiters in nrepl-mode
-(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
