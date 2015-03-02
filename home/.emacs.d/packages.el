f31464aa8cd164334a9f7abf259bd40ee21bf9fc
Mike Callahan <chimpymike@gmail.com>
2015-03-02 10:24:18 -0500

    Pin cider related packages to melpa stable.

--- home/.emacs.d/packages.el
+++ home/.emacs.d/packages.el
@@ -2,9 +2,18 @@
 
 (require 'package)
 
-;; Add melpa repo
+;; Add melpa repos
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
+
+;; Install packages from specific repos
+;; This is essentially for controlling the version of the packages
+(setq package-pinned-packages '((cider . "melpa-stable")
+                                (clojure-mode . "melpa-stable")
+                                (dash . "melpa-stable")
+                                (company . "melpa-stable")))
 
 ;;  Initialize the package system
 (package-initialize)
