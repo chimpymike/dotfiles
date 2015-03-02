3f462c2440d741a169910c824167ab33582c68fd
Mike Callahan <chimpymike@gmail.com>
2015-03-02 10:22:15 -0500

    Use eldoc in clojure buffers.

--- home/.emacs.d/init-scripts/05_clojure.el
+++ home/.emacs.d/init-scripts/05_clojure.el
@@ -9,3 +9,8 @@
 
 ;; Use rainbow-delimiters in nrepl-mode
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+
+;; Use eldoc (function arguments in echo area) in clojure buffers
+(add-hook 'cider-mode-hook #'eldoc-mode)
+;;(add-hook 'clojure-mode-hook 'cider-mode)
+;;(add-hook 'cider-interaction-mode-hook 'cider-turn-on-eldoc-mode)
